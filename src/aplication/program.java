2fd0056265bdc8f083d8e2cd162b291df3729c61
rogerdenardi <rogerde2021@hotmail.com>
2022-02-01 22:24:40 -0300

    teste para informar um número e mostrar na tela

--- src/aplication/program.java
+++ src/aplication/program.java
@@ -1,9 +1,15 @@
 package aplication;
 
+import java.util.Scanner;
+
 public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner (System.in);
+		System.out.println("Informe um número: ");
+		int numero = entrada.nextInt();
+		System.out.println("Número: "+numero);
 
 	}
 
